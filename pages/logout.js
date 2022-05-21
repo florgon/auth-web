031711255b24aed64ff609b01b31fc5d68d61da5
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-21 15:43:44 +0400

    Server-side logout request with `revoke_all`

--- pages/logout.js
+++ pages/logout.js
@@ -1,18 +1,37 @@
 import { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import Head from 'next/head'
+import { authApiRequest } from '@kirillzhosul/florgon-auth-api';
 
-export default function Logout() {
+export function getServerSideProps({ query }) {
+    return {
+        props: { 
+            revoke_all: (query.revoke_all !== undefined)
+        },
+    }
+}
+
+export default function Logout({ revoke_all }) {
     // Usings.
-    const [,,removeCookie] = useCookies([process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_NAME])
+    const [cookies,,removeCookie] = useCookies([process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_NAME])
 
     useEffect(() => {
-        removeCookie(process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_NAME, {
-            "domain": process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_DOMAIN,
-            "maxAge": process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_MAX_AGE,
-            "path": "/"
-        });
-        window.location.replace("/");
+        function logoutBrowser(){
+            removeCookie(process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_NAME, {
+                "domain": process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_DOMAIN,
+                "maxAge": process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_MAX_AGE,
+                "path": "/"
+            });
+            window.location.replace("/");
+        }
+        
+        function sendLogoutRequest(){
+            const sessionToken = cookies[process.env.NEXT_PUBLIC_SESSION_TOKEN_COOKIE_NAME];
+            return authApiRequest("_session._logout", `revoke_all=${revoke_all}&session_token=${sessionToken}`);
+        }
+
+        sendLogoutRequest().then(logoutBrowser).catch(logoutBrowser);
+
     }, [removeCookie])
 
     return (<>
@@ -20,11 +39,7 @@ export default function Logout() {
             <meta name="title" content="Logout" />
             <title>Logout</title>
         </Head>
-        <div className="display-1 text-danger"><b>Logging out...</b></div>
-            <div className="row mt-5 mb-5">
-                <div className="col-lg ml-lg-5 text-center">
-                <p>Please wait...</p>
-            </div>
-        </div>
+        <h3>You are being logouted...</h3>
+        <small>Please wait, logging you out{revoke_all && " of all devices"}...</small>
     </>)
 }
